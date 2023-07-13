f7e39bdba4824356abca93ffbf51ae055def0477
secondl1ght <secondl1ght@protonmail.com>
2023-07-12 21:27:23 -0600

    add comp alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: [{ find: 'comp', replacement: 'src/components/index.js' }]
+	}
 });
