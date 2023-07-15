5fb7c00da82b54d0eed36e8a57868a44342529d9
secondl1ght <secondl1ght@protonmail.com>
2023-07-15 11:27:13 -0600

    change path alias config

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		alias: { comp: 'src/components/index.ts' },
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
