4abcef386ab84afeb3e0d9f0fbeaf7f0c984b035
secondl1ght <secondl1ght@protonmail.com>
2023-07-16 06:58:13 -0600

    add feather icons and comp

---
+++ src/components/Icon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let width = '24';
+	export let height = '24';
+	export let icon: string;
+	export let style = '';
+</script>
+
+<svg
+	class={style}
+	{width}
+	{height}
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<use href="/icons/feather-sprite.svg#{icon}" />
+</svg>
