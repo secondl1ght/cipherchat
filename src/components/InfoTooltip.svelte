4d023d746b658c4b866ee2d83b9525dbdbe0c437
secondl1ght <secondl1ght@protonmail.com>
2023-07-16 06:59:16 -0600

    add infotooltip comp

---
+++ src/components/InfoTooltip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let text: string;
+
+	import tippy from 'tippy.js';
+	import { Icon } from 'comp';
+
+	let infoTooltip: HTMLSpanElement;
+
+	$: infoTooltip &&
+		tippy([infoTooltip], {
+			content: text
+		});
+</script>
+
+<span class="ml-3 cursor-help" bind:this={infoTooltip}>
+	<Icon icon="info" />
+</span>
+
+<style>
+	@import 'tippy.js/dist/tippy.css';
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,6 @@ export { default as Hero } from './Hero.svelte';
 export { default as Login } from './Login.svelte';
 export { default as Connect } from './Connect.svelte';
 export { default as Footer } from './Footer.svelte';
+export { default as Icon } from './Icon.svelte';
+export { default as InfoTooltip } from './InfoTooltip.svelte';
+export { default as FormWrapper } from './FormWrapper.svelte';
