2f9510f431ac19cdb8b4fc4700f1368c120f1ecd
secondl1ght <secondl1ght@protonmail.com>
2023-07-27 16:55:30 -0600

    add user node info to store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,3 +3,6 @@ import { lnc } from '$lib/lnc';
 
 export const connected = writable(false);
 export const paired = writable(lnc.credentials.isPaired);
+export const pubkey = writable('');
+export const alias = writable('');
+export const color = writable('');
