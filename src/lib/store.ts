f6f6a400166881c168873fc370545ea91fedf26b
secondl1ght <secondl1ght@protonmail.com>
2023-08-19 21:10:40 -0600

    use better naming for user stores

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,17 +4,9 @@ import { readable, writable } from 'svelte/store';
 export const paired = writable(lnc.credentials.isPaired);
 export const connected = writable(false);
 export const cryptoKey = writable();
-export const pubkey = writable('');
-export const alias = writable('');
-export const color = writable('');
-export const TLV_RECORDS = readable({
-	KEYSEND_PREIMAGE: 5482373484,
-	SENDERS_PUBKEY: 34349339,
-	TIMESTAMP: 34349343,
-	MESSAGE_CONTENT: 34349334,
-	SIGNATURE: 34349337,
-	CONTENT_TYPE: 34349345
-});
+export const userPubkey = writable('');
+export const userAlias = writable('');
+export const userColor = writable('');
 export const addConvo = writable('');
 export const activeConversation = writable('');
 export const lockMessage = writable('');
