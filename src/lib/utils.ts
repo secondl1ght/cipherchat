808274b8615bf55f04b0cfa4714598d90acb8169
secondl1ght <secondl1ght@protonmail.com>
2023-08-30 17:58:03 -0600

    getrandomcolor util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -59,3 +59,12 @@ export const shortenPubkey = (pubkey: string) =>
 export const formatNumber = (number: number) => new Intl.NumberFormat().format(number);
 
 export const getTimestamp = () => Date.now() * 1000000;
+
+export function getRandomColor() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
