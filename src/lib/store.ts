3795db79bed9584e95aa89e2ebaf7c267613839b
secondl1ght <secondl1ght@protonmail.com>
2023-08-30 20:40:53 -0600

    organize store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,19 +4,26 @@ import { readable, writable, type Writable } from 'svelte/store';
 
 export const paired = writable(lnc.credentials.isPaired);
 export const connected = writable(false);
+
 export const cryptoKey: Writable<CryptoKey | undefined> = writable();
+
 export const userPubkey = writable('');
 export const userAlias = writable('');
 export const userColor = writable('');
 export const userAvatar = writable(localStorage.getItem('userAvatar'));
+
 export const appView = writable(AppViewState.Home);
+export const homeState: Writable<'HOME' | 'PROFILE' | 'ADD'> = writable('HOME');
+
 export const conversations: Writable<Conversation[]> = writable();
-export const messages: Writable<MessageDecrypted[]> = writable();
-export const messageHistory = writable(25);
 export const addConvo: Writable<'' | 'LOADING' | 'SUCCESS'> = writable('');
 export const activeConversation = writable('');
+
+export const messages: Writable<MessageDecrypted[]> = writable();
+export const messageHistory = writable(25);
 export const lockMessage = writable(false);
 export const clearMessage = writable(false);
+
 export const links = readable({
 	github: 'https://github.com/secondl1ght/cipherchat',
 	twitter: '',
@@ -24,4 +31,5 @@ export const links = readable({
 	matrix: '',
 	rumble: ''
 });
+
 export const error = writable({ status: '', message: '' });
