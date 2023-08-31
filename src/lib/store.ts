ff905a8ccc3a17f62148080f7a393b6d0012dd96
secondl1ght <secondl1ght@protonmail.com>
2023-08-30 18:06:14 -0600

    add store types and new stores

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,16 +1,22 @@
 import { lnc } from '$lib/lnc';
-import { readable, writable } from 'svelte/store';
+import { AppViewState, type Conversation, type MessageDecrypted } from '$lib/types';
+import { readable, writable, type Writable } from 'svelte/store';
 
 export const paired = writable(lnc.credentials.isPaired);
 export const connected = writable(false);
-export const cryptoKey = writable();
+export const cryptoKey: Writable<CryptoKey | undefined> = writable();
 export const userPubkey = writable('');
 export const userAlias = writable('');
 export const userColor = writable('');
-export const addConvo = writable('');
+export const userAvatar = writable(localStorage.getItem('userAvatar'));
+export const appView = writable(AppViewState.Home);
+export const conversations: Writable<Conversation[]> = writable();
+export const messages: Writable<MessageDecrypted[]> = writable();
+export const messageHistory = writable(25);
+export const addConvo: Writable<'' | 'LOADING' | 'SUCCESS'> = writable('');
 export const activeConversation = writable('');
-export const lockMessage = writable('');
-export const clearMessage = writable('');
+export const lockMessage = writable(false);
+export const clearMessage = writable(false);
 export const links = readable({
 	github: 'https://github.com/secondl1ght/cipherchat',
 	twitter: '',
