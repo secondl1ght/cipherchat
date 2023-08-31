8404c3f0dbb51ebcafe944414e16269850310102
secondl1ght <secondl1ght@protonmail.com>
2023-08-30 18:09:10 -0600

    setup convo view

---
+++ src/components/ConvoView.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { ChatWindow, MessageInfo, Node } from 'comp';
+
+	let state: 'CHAT' | 'NODE' | 'MESSAGE' = 'CHAT';
+</script>
+
+<section class="h-full w-full border-borderIn p-4 lg:border-l-8">
+	{#if state === 'CHAT'}
+		<ChatWindow />
+	{:else if state === 'NODE'}
+		<Node />
+	{:else if state === 'MESSAGE'}
+		<MessageInfo />
+	{/if}
+</section>
