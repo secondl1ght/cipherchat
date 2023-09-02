670e63fad3369baa0be78ba457439691310429ad
secondl1ght <secondl1ght@protonmail.com>
2023-09-01 20:14:22 -0600

    create copy btn comp

---
+++ src/components/CopyButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let text: string;
+	export let width = '24';
+	export let height = '24';
+
+	import { copy } from '$lib/utils';
+	import { Icon } from 'comp';
+
+	let copied = false;
+</script>
+
+<button
+	on:click={() => {
+		copy(text);
+		copied = true;
+		setTimeout(() => (copied = false), 2100);
+	}}
+	disabled={copied}
+>
+	<Icon
+		icon={copied ? 'check' : 'clipboard'}
+		style="text-header hover:text-button transition-colors"
+		{width}
+		{height}
+	/>
+</button>
