1583d5bbe1e0ef46f4acc425fa460f5f9dd6514f
secondl1ght <secondl1ght@protonmail.com>
2023-09-03 16:32:38 -0600

    new helper functions for time and msg formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -56,9 +56,31 @@ export const setError = (status: string, message: string) => error.set({ status,
 export const shortenPubkey = (pubkey: string) =>
 	pubkey.slice(0, 6) + '...' + pubkey.slice(pubkey.length - 6, pubkey.length);
 
+export const shortenAlias = (alias: string | undefined) => {
+	if (!alias || alias.length <= 20) return alias;
+
+	return alias.slice(0, 20) + '...';
+};
+
+export const shortenLatestMessage = (message: string, limit: number) => {
+	if (message.length <= limit) return message;
+
+	return message.slice(0, limit) + '...';
+};
+
 export const formatNumber = (number: number) => new Intl.NumberFormat().format(number);
 
 export const getTimestamp = () => Date.now() * 1000000;
+
+export const formatTimestamp = (timestamp: number) => {
+	const milliseconds = timestamp / 1000000;
+	const date = new Date(milliseconds);
+
+	return {
+		date: new Intl.DateTimeFormat('en-US', { dateStyle: 'short' }).format(date),
+		time: new Intl.DateTimeFormat('en-US', { timeStyle: 'short' }).format(date)
+	};
+};
 
 export function getRandomColor() {
 	const letters = '0123456789ABCDEF';
