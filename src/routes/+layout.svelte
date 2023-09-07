054b14b5ce7009d31f6bf301ed1faa94bb1ded65
secondl1ght <secondl1ght@protonmail.com>
2023-09-07 11:54:22 -0600

    move toast to bottom on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { innerWidth } from '$lib/store';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import { onMount } from 'svelte';
 	import '../app.css';
 
 	const options = {
-		pausable: true
+		pausable: true,
+		reversed: $innerWidth < 640 ? true : false,
+		intro: $innerWidth < 640 ? { y: 192 } : undefined
 	};
 
 	onMount(() =>
@@ -20,5 +23,14 @@
 <style>
 	#toast {
 		font-family: Roboto, sans-serif;
+	}
+
+	@media (max-width: 640px) {
+		#toast {
+			--toastContainerTop: auto;
+			--toastContainerRight: auto;
+			--toastContainerBottom: 8rem;
+			--toastContainerLeft: calc(50vw - 8rem);
+		}
 	}
 </style>
