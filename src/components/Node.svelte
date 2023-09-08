beba329a69dcf73d7655b6074db9178844931e74
secondl1ght <secondl1ght@protonmail.com>
2023-09-08 12:58:33 -0600

    node page

--- src/components/Node.svelte
+++ src/components/Node.svelte
@@ -1,2 +1,179 @@
 <script lang="ts">
+	import { bufferHexToBase64 } from '$lib/buffer';
+	import { db } from '$lib/db';
+	import { lnc } from '$lib/lnc';
+	import { activeConversation, conversation } from '$lib/store';
+	import {
+		errorToast,
+		formatNumber,
+		shortenAddress,
+		shortenPubkey,
+		successToast
+	} from '$lib/utils';
+	import type { lnrpc } from '@lightninglabs/lnc-web';
+	import { Avatar, CopyButton, InfoTooltip, RowItem } from 'comp';
+	import { onMount } from 'svelte';
+	import SharedChannel from './SharedChannel.svelte';
+
+	let nodeInfo: lnrpc.NodeInfo;
+	let channels: lnrpc.Channel[] = [];
+
+	let loading = true;
+
+	let charLimit = $conversation.charLimit;
+
+	onMount(async () => {
+		try {
+			nodeInfo = await lnc.lnd.lightning.getNodeInfo({
+				pubKey: $activeConversation,
+				includeChannels: false
+			});
+		} catch (error) {
+			console.log(error);
+			errorToast('Could not fetch node information, please try again.');
+		}
+
+		try {
+			const response = await lnc.lnd.lightning.listChannels({
+				peer: bufferHexToBase64($activeConversation)
+			});
+
+			channels = response.channels;
+
+			loading = false;
+		} catch (error) {
+			console.log(error);
+			errorToast('Could not fetch channels, please try again.');
+		}
+	});
 </script>
+
+{#if $conversation}
+	<div class="space-y-4">
+		<div class="mx-auto w-fit">
+			<Avatar
+				pubkey={$conversation.pubkey}
+				alias={$conversation.alias}
+				color={$conversation.color}
+				avatar={$conversation.avatar}
+				size="w-20 h-20"
+				style="cursor-default"
+				withButton
+			/>
+		</div>
+
+		<h1 class="press-start break-all text-center text-header md:text-lg">
+			{$conversation.alias || shortenPubkey($conversation.pubkey)}
+		</h1>
+
+		<ul class="space-y-4">
+			<RowItem title="Pubkey">
+				<p class="flex items-center space-x-2">
+					<CopyButton text={$conversation.pubkey} width="16" height="16" />
+					<span class="inline-block">
+						{shortenPubkey($conversation.pubkey)}
+					</span>
+				</p>
+			</RowItem>
+
+			<RowItem title="Color">
+				<p style:color={$conversation.color}>{$conversation.color}</p>
+			</RowItem>
+
+			<RowItem title="Channels">
+				<p class={nodeInfo ? 'cursor-auto' : 'cursor-wait'}>
+					{nodeInfo?.numChannels !== undefined ? formatNumber(nodeInfo.numChannels) : '-'}
+				</p>
+			</RowItem>
+
+			<RowItem title="Capacity">
+				<p class="whitespace-nowrap {nodeInfo ? 'cursor-auto' : 'cursor-wait'}">
+					{nodeInfo?.totalCapacity !== undefined
+						? formatNumber(Number(nodeInfo.totalCapacity))
+						: '-'} sats
+				</p>
+			</RowItem>
+
+			<RowItem title="Last Update">
+				<p class="whitespace-nowrap {nodeInfo ? 'cursor-auto' : 'cursor-wait'}">
+					{nodeInfo?.node?.lastUpdate !== undefined
+						? new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short' }).format(
+								new Date(nodeInfo.node.lastUpdate * 1000)
+						  )
+						: '-'}
+				</p>
+			</RowItem>
+
+			<RowItem title="Addresses">
+				{#if nodeInfo?.node}
+					<ol class="space-y-2">
+						{#each nodeInfo.node.addresses as address}
+							<li class="flex items-center space-x-2">
+								<CopyButton text={address.addr} width="16" height="16" />
+								<span class="inline-block">
+									{shortenAddress(address.addr)}
+								</span>
+							</li>
+						{/each}
+					</ol>
+				{:else}
+					<p class="cursor-wait">-</p>
+				{/if}
+			</RowItem>
+
+			<RowItem title="Char Limit" label="char-limit">
+				<div class="flex items-center">
+					<InfoTooltip
+						style="mr-3 text-header"
+						text="There are limitations to the size of messages that can be sent over the lightning network, depending on the length of the route."
+					/>
+
+					<div>
+						<p class="text-center font-bold">{charLimit}</p>
+
+						<input
+							bind:value={charLimit}
+							on:change={async () => {
+								try {
+									await db.transaction('rw', db.conversations, async () => {
+										await db.conversations.update($activeConversation, { charLimit });
+									});
+
+									successToast('Setting updated.');
+								} catch (error) {
+									console.log(error);
+									errorToast('Could not update char limit, please try again.');
+								}
+							}}
+							type="range"
+							id="char-limit"
+							name="char-limit"
+							min="100"
+							max="1000"
+							step="100"
+							class="cursor-pointer accent-button"
+						/>
+					</div>
+				</div>
+			</RowItem>
+		</ul>
+
+		<hr class="border-header" />
+
+		<div class="space-y-4">
+			<h2 class="text-center text-header">Shared Channels</h2>
+
+			{#if channels.length}
+				<div class="space-y-4">
+					{#each channels as channel}
+						<SharedChannel {channel} />
+					{/each}
+				</div>
+			{:else if loading}
+				<p class="text-center">Loading...</p>
+			{:else}
+				<p class="text-center">You don't have any channels open with this node.</p>
+			{/if}
+		</div>
+	</div>
+{/if}
---
+++ src/components/SharedChannel.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	export let channel: lnrpc.Channel;
+
+	import { conversation, userColor } from '$lib/store';
+	import { formatNumber } from '$lib/utils';
+	import type { lnrpc } from '@lightninglabs/lnc-web';
+	import { CopyButton, RowItem } from 'comp';
+	import numbro from 'numbro';
+	import tippy from 'tippy.js';
+
+	let localBtn: HTMLButtonElement;
+	let remoteBtn: HTMLButtonElement;
+
+	const local = Number(channel.localBalance);
+	const remote = Number(channel.remoteBalance);
+
+	const total = local + remote;
+	const localPercent = ((local / total) * 100).toFixed(0) + '%';
+	const remotePercent = ((remote / total) * 100).toFixed(0) + '%';
+
+	$: localBtn &&
+		tippy([localBtn], {
+			content: formatNumber(local)
+		});
+
+	$: remoteBtn &&
+		tippy([remoteBtn], {
+			content: formatNumber(remote)
+		});
+
+	let sideHover: 'LOCAL' | 'REMOTE' | undefined;
+</script>
+
+<div class="space-y-2">
+	<p class="flex items-center justify-center space-x-2">
+		<span class="relative flex h-3 w-3">
+			<span
+				class="absolute inline-flex h-full w-full rounded-full opacity-75 {channel.active
+					? 'animate-ping bg-success'
+					: 'bg-error'}"
+			/>
+			<span
+				class="relative inline-flex h-3 w-3 rounded-full {channel.active
+					? 'bg-success'
+					: 'bg-error'}"
+			/>
+		</span>
+
+		<span class="inline-block font-bold">{channel.chanId}</span>
+
+		<CopyButton text={channel.chanId} width="16" height="16" />
+	</p>
+
+	<div class="flex justify-center">
+		<div class="w-1/2">
+			<p class="mb-2 text-center">Local</p>
+
+			<button
+				bind:this={localBtn}
+				on:mouseenter={() => (sideHover = 'LOCAL')}
+				on:mouseleave={() => (sideHover = undefined)}
+				class="relative flex w-full justify-end border border-header bg-borderIn {sideHover ===
+				'REMOTE'
+					? 'opacity-50'
+					: ''} transition-opacity"
+			>
+				<div
+					style:width={localPercent}
+					style:background-color={$userColor || '#F7931A'}
+					class="h-6 text-center text-header"
+				>
+					<div class="sats-label">{numbro(local).format({ average: true, mantissa: 1 })}</div>
+				</div>
+			</button>
+		</div>
+
+		<div class="w-1/2">
+			<p class="mb-2 text-center">Remote</p>
+			
+			<button
+				bind:this={remoteBtn}
+				on:mouseenter={() => (sideHover = 'REMOTE')}
+				on:mouseleave={() => (sideHover = undefined)}
+				class="relative w-full border border-header bg-borderIn {sideHover === 'LOCAL'
+					? 'opacity-50'
+					: ''} transition-opacity"
+			>
+				<div
+					style:width={remotePercent}
+					style:background-color={$conversation.color || '#F7931A'}
+					class="h-6 text-center text-header"
+				>
+					<div class="sats-label">{numbro(remote).format({ average: true, mantissa: 1 })}</div>
+				</div>
+			</button>
+		</div>
+	</div>
+
+	<RowItem title="Capacity">
+		<p class="whitespace-nowrap">{formatNumber(Number(channel.capacity))} sats</p>
+	</RowItem>
+
+	<RowItem title="Total Sent">
+		<p class="whitespace-nowrap">{formatNumber(Number(channel.totalSatoshisSent))} sats</p>
+	</RowItem>
+
+	<RowItem title="Total Received">
+		<p class="whitespace-nowrap">{formatNumber(Number(channel.totalSatoshisReceived))} sats</p>
+	</RowItem>
+</div>
+
+<style>
+	@import 'tippy.js/dist/tippy.css';
+
+	.sats-label {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		-moz-transform: translateX(-50%) translateY(-50%);
+		-webkit-transform: translateX(-50%) translateY(-50%);
+		transform: translateX(-50%) translateY(-50%);
+	}
+</style>
