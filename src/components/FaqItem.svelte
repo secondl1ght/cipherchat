b33418ea3bced6a515e08b057141f878ed3ea205
secondl1ght <secondl1ght@protonmail.com>
2023-11-03 18:06:38 -0600

    add links to faq items

--- src/components/FaqItem.svelte
+++ src/components/FaqItem.svelte
@@ -1,8 +1,35 @@
 <script lang="ts">
+	import { Icon } from 'comp';
+
 	export let question: string;
+
+	const hash = question
+		.replaceAll(' ', '-')
+		.replaceAll('?', '')
+		.replaceAll(',', '')
+		.replaceAll("'", '')
+		.toLowerCase();
+
+	let showIcon = false;
 </script>
 
-<li>
-	<h4 class="mb-2 text-header">{question}</h4>
+<li id={hash}>
+	<div class="mb-2 flex items-center space-x-4">
+		<a
+			href={'#' + hash}
+			class="inline-block"
+			on:mouseenter={() => (showIcon = true)}
+			on:mouseleave={() => (showIcon = false)}
+		>
+			<h4 class="text-header">
+				{question}
+			</h4>
+		</a>
+
+		{#if showIcon}
+			<Icon icon="link-2" style="min-w-[16px] min-h-[16px]" width="16" height="16" />
+		{/if}
+	</div>
+
 	<p><slot /></p>
 </li>
