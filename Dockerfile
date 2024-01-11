4990cc530d04e8d8f1cc1e9bbca00cfd6b487d4b
secondl1ght <secondl1ght@protonmail.com>
2024-01-10 22:16:11 -0700

    add adapter node and update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,17 @@ FROM node:lts AS builder
 WORKDIR /app
 COPY package.json yarn.lock ./
 RUN yarn
+ENV ADAPTER_NODE=true
 COPY . .
 RUN yarn build
+RUN yarn --prod
 
 FROM node:lts
 
-USER 1000
 WORKDIR /app
-COPY --from=builder /app/build .
+COPY --from=builder /app/build build/
+COPY package.json .
+COPY --from=builder /app/node_modules node_modules/
+ENV NODE_ENV=production
 EXPOSE 3000
-CMD ["node", "./index.js"]
+CMD ["node", "build"]
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import { default as adapterNode } from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
@@ -19,7 +20,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: process.env.ADAPTER_NODE ? adapterNode() : adapter(),
 		serviceWorker: { register: false }
 	}
 };
